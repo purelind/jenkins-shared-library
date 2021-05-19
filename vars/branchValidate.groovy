755b603b8d9a206cbe88456609b056057a96a730
purelind <purelind@gmail.com>
2021-05-19 13:57:13 +0800

    add isBranchMatched

--- vars/branchValidate.groovy
+++
@@ -1,9 +0,0 @@
-boolean call(List<String> branchs, String targetBranch) {
-    branchs.each {branch ->
-        println branch
-        if (targetBranch.startsWith(branch)) {
-            return true
-        }
-    }
-    return false
-}
---
+++ vars/isBranchMatched.groovy
@@ -0,0 +1,9 @@
+boolean call(List<String> branches, String targetBranch) {
+    for (String item : branches) {
+        if (targetBranch.startsWith(item)) {
+            println "targetBranch=${targetBranch} matched in ${branches}"
+            return true
+        }
+    }
+    return false
+}
